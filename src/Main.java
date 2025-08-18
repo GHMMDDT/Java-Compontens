96948abca7c1296649e8d51c44f14aa7651fa932
Other-User1 <otheruser.iapv@gmail.com>
2025-08-18 13:50:28 -0600

    Fixing & Update Archive/Folder

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java Components");
 		System.out.println();
-		System.out.println("Created by: MDDT (GHMMDDT)");
+		System.out.println("Created by: MDDT (Modification design Developer Team) powered by: GHMMDDT | 1.0 Alpha");
 	}
 }
