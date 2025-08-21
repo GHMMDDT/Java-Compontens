5429af466ba941d3bc9d0b9ec6526011106a2a15
Other-User1 <otheruser.iapv@gmail.com>
2025-08-20 21:55:14 -0600

    Fixing & Update Archive/Folder

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,5 @@ public class Main {
 		System.out.println("Welcome to Java Components");
 		System.out.println();
 		System.out.println("Created by: MDDT (Modification design Developer Team) powered by: GHMMDDT | 1.0 Alpha");
-
-		long start = System.currentTimeMillis();
-		CharArrayBuilder target = new CharArrayBuilder();
-		for (int i = 0; i < 100_000; i++) target.append("Hello, Wordl!");
-		target.indexOfAll(new CharArrayBuilder.OnIndexListener() {
-			@Override
-			public boolean onIndex(char current) {
-				if (current == ' ' || current == ',' || current == '!') return true;
-				return false;
-			}
-		});
-		long end = System.currentTimeMillis();
-
-		System.out.println("Time: " + (end - start) + "ms"); // Output: Time: 40900ms
 	}
 }
